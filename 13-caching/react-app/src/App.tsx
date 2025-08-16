cfba9cc7d7b62bf2e1a39649e497875c5a12f2f4
Javiro Chacon <chaconjaviro@yahoo.com>
2025-08-16 11:51:36 -0600

    lint wanrn ok

--- 13-caching/react-app/src/App.tsx
+++ 13-caching/react-app/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const pi = 3.14;
+  //const pi = 3.14;
   return (
     <div className="App">
       <header className="App-header">
